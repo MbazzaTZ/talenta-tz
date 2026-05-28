3cf4577976b37d984becf0001cbc05b9d2fd5fa4
Mbazza <mbazza@example.com>
2026-05-28 16:16:47 +0300

    Hotfix: Deploy working static version to stop 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 ﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterPlugin } from "@tanstack/router-plugin";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterPlugin({
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-    }),
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 3000,
   },
 });
